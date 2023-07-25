493f2dffc16c53842d186f8feab714b264860e1d
minutemancarlo <67291699+minutemancarlo@users.noreply.github.com>
2023-07-25 15:50:06 +0800

    Updated signupController
    Fix issue on SSL Certificate Verification.

--- controllers/signupController.php
+++ controllers/signupController.php
@@ -59,7 +59,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             curl_setopt($ch, CURLOPT_POST, 1);
             curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
             curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
             // Execute cURL request and get the response
             $response = curl_exec($ch);
 
