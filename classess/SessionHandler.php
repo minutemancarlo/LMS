a0355479b83850848c939c374896650e22bc6a02
minutemancarlo <67291699+minutemancarlo@users.noreply.github.com>
2023-07-29 00:43:49 +0800

    Updated Session Time
    Changed 10 minutes to 30 minutes

--- classess/SessionHandler.php
+++ classess/SessionHandler.php
@@ -1,6 +1,6 @@
 <?php
 class CustomSessionHandler {
-    private $sessionTimeout = 600; // Timeout in seconds - 10 minutes
+    private $sessionTimeout = 1800; // Timeout in seconds - 10 minutes
 
     public function __construct() {
         // Start the session
--- controllers/sessionController.php
+++ controllers/sessionController.php
@@ -4,7 +4,7 @@ require_once '../classess/SessionHandler.php';
 $session=new CustomSessionHandler();
 
 if($session->checkSessionExpirationAsync()){
-  $response = array("success" => true, "message" => "You are inactive for 10 minutes.");
+  $response = array("success" => true, "message" => "You are inactive for 30 minutes.");
   echo json_encode($response);
 }else{
   $response = array("success" => false, "message" => "ok");
